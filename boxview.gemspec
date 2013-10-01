abe6535f959ec3ec1c8ce3457464ad51b4d99c1e
Cyrus Karbassiyoon <ckarbass@gmail.com>
2013-09-30 23:14:03 -0700

    boxview api

--- boxview.gemspec
+++ boxview.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
-  s.name        = 'box'
-  s.version     = '0.0.0'
+  s.name        = 'boxview'
+  s.version     = '0.0.1'
   s.date        = '2013-09-28'
   s.summary     = "Wrapper for box view api"
   s.description = "Wrapper for box view api"
   s.authors     = ["Cyrus Karbassiyoon"]
   s.email       = 'ckarbass@gmail.com'
-  s.files       = ["lib/box/view.rb"]
+  s.files       = ["lib/boxview.rb"]
   s.add_development_dependency "faraday"
   s.add_development_dependency "faraday_middleware"
 end
--- lib/boxview.rb
+++ lib/boxview.rb
@@ -1,36 +1,35 @@
 require 'faraday_middleware'
 
-module Box
-  module View
-    def self.init token
-      @@api_token = token 
-    end
+module BoxView
+  def self.init token
+    @@api_token = token 
+  end
 
-    def self.api_url
-      "https://view-api.box.com/1"
-    end
+  def self.api_url
+    "https://view-api.box.com/1"
+  end
 
-    def self.conn
-      Faraday.new(:url => self.api_url) do |conn|
-        conn.basic_auth @@api_id, @@api_secret
-        conn.request  :url_encoded # convert request params as "www-form-urlencoded"
-        conn.response :mashify
-        conn.response :json, :content_type => /\bjson$/
-        conn.adapter Faraday.default_adapter
+  def self.connection
+    connection = Faraday.new(:url => BoxView.api_url) do |conn|
+      #conn.request  :url_encoded # convert request params as "www-form-urlencoded"
+      conn.response :mashify
+      conn.response :json, :content_type => /\bjson$/
+      conn.adapter Faraday.default_adapter
+    end
+    connection.headers['Authorization'] = "Token #{@@api_token}"
+    connection.headers['Content-Type'] = 'application/json'
+    return connection
+  end
+
+  module Documents
+    @@endpoint = "documents"
+
+    def self.create url 
+      BoxView.connection.post do |request|
+        request.url @@endpoint
+        request.body = '{"url": "' + url + '"}'
       end
     end
 
-    module Documents
-
-      def create url 
-        Box::View.connection.post do |req|
-          req.url 'documents'
-          req.headers['Content-Type'] = 'application/json'
-          req.headers['Authorization'] = "Token #{Box::View.token}"
-          req.body = '{"url": "' + url + '"}'
-        end
-      end
-
-    end
   end
 end
