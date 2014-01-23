146c075cd6e9d395c3a7e9a49ad79271e049f21b
Cyrus Karbassiyoon <ckarbass@gmail.com>
2014-01-22 22:09:30 -0800

    adding thumbnail call

--- lib/boxview.rb
+++ lib/boxview.rb
@@ -20,12 +20,10 @@ module BoxView
   end
 
   module Documents
-    @@endpoint = "documents"
-
     # url: url of a document in the cloud
     def self.create url
       BoxView.connection.post do |request|
-        request.url @@endpoint
+        request.url "documents"
         request.body = '{"url": "' + url + '"}'
       end
     end
@@ -43,5 +41,41 @@ module BoxView
       BoxView.connection.get "documents/#{id}/content.zip"
     end
 
+    def self.get_thumbnail id, options
+      BoxView.connection.get "documents/#{id}/thumbnail?width=#{options[:width]}&height=#{options[:height]}"
+    end
+
   end
+
+=begin
+  module Sessions
+    @@endpoint = "sessions"
+
+    def self.create document_id
+      BoxView.connection.post do |request|
+        request.url @@endpoint
+        request.body = '{"document_id": "' + document_id + '"}'
+      end
+    end
+  end
+
+  # Gets a session to view the link, should be in box api wrapper
+  def get_box_session
+    box_url = 'https://view-api.box.com/1'
+    connection = Faraday.new box_url do |conn|
+      conn.request :json
+      conn.response :json, :content_type => /\bjson$/
+      conn.adapter Faraday.default_adapter
+    end
+
+    result = connection.post do |req|
+      req.url 'sessions'
+      req.headers['Content-Type'] = 'application/json'
+      req.headers['Authorization'] = "Token SsLuBXaj1bGczeOtD9VxIYl0QpNC3ndM"
+      req.body = '{"document_id": "' + self.document.box_id + '"}'
+    end
+    result.env[:body]["id"]
+  end
+=end
+
 end
